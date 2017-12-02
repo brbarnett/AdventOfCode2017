485217d3ee1fe6c62e3626f28b38332bb98e78dd
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-12-02 11:08:35 -0600

    More elegant solution to 2b

--- 2b.js
+++ 2b.js
@@ -3,10 +3,10 @@ const fs = require('fs'),
 
 class Solution {
     run(test) {
-        const input = fs.readFileSync('./2.dat', 'utf8')
+        const input = fs.readFileSync('./2.dat', 'utf8');
 
         const result = this.checksum(input);
-        console.log('Result:', result);
+        console.log('Result:', result); // returns 294 for final answer
     }
 
     checksum(input) {
@@ -20,16 +20,27 @@ class Solution {
                     .map(x => +x)   // convert all to number
                     .value();
 
-                let divisor = undefined;
-                let numerator = _.find(data, (x) => {
-                    divisor = _.find(data, (y) => x !== y && x % y === 0);
-                    return divisor;
-                });
-                result += numerator / divisor;
+                result += +_(this.cartesianProductOf(data, data))
+                    .chain()
+                    .filter(x => x[0] !== x[1]) // shortcut to avoid self/self
+                    .filter(x => x[0] % x[1] === 0) // check where (a) evenly divisible by (b)
+                    .map(x => x[0] / x[1])  // divide (a) by (b)
+                    .value();
                 return result;
             }, 0)
             .value()
     }
+
+    // reference: https://stackoverflow.com/questions/12303989/cartesian-product-of-multiple-arrays-in-javascript
+    cartesianProductOf() {
+        return _.reduce(arguments, function (a, b) {
+            return _.flatten(_.map(a, function (x) {
+                return _.map(b, function (y) {
+                    return x.concat([y]);
+                });
+            }), true);
+        }, [[]]);
+    };
 }
 
 module.exports = Solution;
