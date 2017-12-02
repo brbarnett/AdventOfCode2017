81e364fa819fe870eba37a5be5edc08424ff176b
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-12-02 11:29:02 -0600

    Custom lodash functionality - debug

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const _ = require('lodash');
 const process = require('./2b');
-
-new process().run(test);
 
 function test(input, expected, eval) {
     let output = eval(input);
 
     console.log('Input:', input, '; Calculated:', output, '; Expected:', expected, '; Pass:', output === expected)
 }
+
+// custom lodash methods
+(function() {
+    _.mixin({
+        'debug': function (val) {
+            console.log(val);
+            return val;
+        }
+    });
+}());
+
+new process().run(test);
