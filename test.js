01fe09d2b4ce13ac1aeea6f4fa09a7a0817ffb00
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-12-03 13:43:13 -0600

    3b tests

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 // Day 1
-require('./1a.tests');
-require('./1b.tests');
+//require('./1a.tests');
+//require('./1b.tests');
 
 // Day 2
-require('./2a.tests');
-require('./2b.tests');
+//require('./2a.tests');
+//require('./2b.tests');
+
+// Day 3
+//require('./3a.tests');
+require('./3b.tests');
