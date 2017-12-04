7fef66e81a5df3c9391af67a4e2c5b0a3f89bc2f
Brandon R. Barnett <barnett@echosolutionsgroup.com>
2017-12-04 07:47:12 -0600

    Solution to 4a

--- 4a.js
+++ 4a.js
@@ -1,15 +1,26 @@
-const _ = require('lodash');
+const fs = require('fs'),
+    _ = require('lodash');
 
 class Solution {
     run() {
         const input = fs.readFileSync('./4.dat', 'utf8');
 
         const result = this.solve(input);
-        console.log('Result:', result);
+        console.log('Result:', result); // correct answer is 451
     }
 
     solve(input) {
-        return input;
+        return _(input)
+            .chain()
+            .split('\r\n')    // split input by lines
+            .reduce((total, next) => {
+                const arr = _(next).split(' ').value(); // convert to array of 'words'
+
+                if (arr.length === _.uniq(arr).length) total++; // check if array word length differs from array of unique words
+
+                return total;
+            }, 0)
+            .value();
     }
 
 }
--- 4a.tests.js
+++ 4a.tests.js
@@ -4,7 +4,7 @@ const solution = new (require('./4a'))(),
 const assert = require('assert');
 
 describe('4a', () => {
-    testHelpers.stringReturnsNumber('aa bb cc dd ee', true, solution.solve.bind(solution));
-    testHelpers.stringReturnsNumber('aa bb cc dd aa', false, solution.solve.bind(solution));
-    testHelpers.stringReturnsNumber('aa bb cc dd aaa', true, solution.solve.bind(solution));
+    testHelpers.stringReturnsNumber('aa bb cc dd ee', 1, solution.solve.bind(solution));
+    testHelpers.stringReturnsNumber('aa bb cc dd aa', 0, solution.solve.bind(solution));
+    testHelpers.stringReturnsNumber('aa bb cc dd aaa', 1, solution.solve.bind(solution));
 });
